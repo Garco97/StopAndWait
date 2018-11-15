85238b18f7fdca9e3d3b974ee656846b9b85030e
DIEGO <cosiod@CZC7318387.ciencias.unican.es>
2018-11-15 17:27:11 +0100

    Ultima modificacion durante la clase (sigue sin pintar correctamente los mensajes, añade unos caracteres desconocidos)

--- flowcontrol/eventCallbacks.c
+++ flowcontrol/eventCallbacks.c
@@ -16,11 +16,11 @@
 #include "global.h"
 
 /*
- * SECTION 1: GLOBAL DATA
- * ----------------------
- * Add your own data fields below this
- *
- */
+* SECTION 1: GLOBAL DATA
+* ----------------------
+* Add your own data fields below this
+*
+*/
 
 int window_size;
 long timeout;
@@ -28,20 +28,20 @@ void* buffer[500];
 int chk_validation;
 
 /*
- * SECTION 2: CALLBACK FUNCTIONS
- * -----------------------------
- * The following functions are called on the corresponding event
- *
- */
+* SECTION 2: CALLBACK FUNCTIONS
+* -----------------------------
+* The following functions are called on the corresponding event
+*
+*/
 
 
 /*
- * Creates a new connection. You should declare any variable needed in the upper section, DO NOT DECLARE
- * ANY VARIABLES HERE. This function should only make initializations as required.
- *
- * You should save the input parameters in some persistant global variable (defined in Section 1) so it can be
- * accessed later
- */
+* Creates a new connection. You should declare any variable needed in the upper section, DO NOT DECLARE
+* ANY VARIABLES HERE. This function should only make initializations as required.
+*
+* You should save the input parameters in some persistant global variable (defined in Section 1) so it can be
+* accessed later
+*/
 void connection_initialization(int _windowSize, long timeout_in_ns) {
   window_size = _windowSize;
   timeout = timeout_in_ns;
@@ -52,28 +52,36 @@ void connection_initialization(int _windowSize, long timeout_in_ns) {
 void receive_callback(packet_t *pkt, size_t n) {
   int chk_validation = VALIDATE_CHECKSUM(pkt);
   if (chk_validation == 0){
-    printf("%s\n", "Wrong");
+    printf("%s\n", "Wrong checksum");
   }else{
-    printf("%s\n", "Correct");
+    int size = pkt->len;
+    int tam = ACCEPT_DATA(pkt->data,size);
+    if(ACCEPT_DATA(pkt->data,size) == -1){
+      printf("%s\n", "Wrong data");
+    }
+
   }
-  int size = pkt->len;
-  ACCEPT_DATA(buffer,size);
+
 }
 
 /* Callback called when the application wants to send data to the other end*/
 void send_callback() {
   int prueba = READ_DATA_FROM_APP_LAYER(buffer,500);
-  packetType_t type = DATA;
-  uint32_t ackNo = 1;
-  uint32_t seqNo = 1;
-  uint16_t length = 100;
-  SEND_DATA_PACKET(type,length,ackNo, seqNo, &buffer);
+  if (prueba == -1){
+    printf("%s\n","ERROR READ" );
+  }else{
+    packetType_t type = DATA;
+    uint32_t ackNo = 1;
+    uint32_t seqNo = 1;
+    uint16_t length = prueba + 10;
+    SEND_DATA_PACKET(type,length,ackNo, seqNo, &buffer);
+  }
 }
 
 /*
- * This function gets called when timer with index "timerNumber" expires.
- * The function of this timer depends on the protocol programmer
- */
+* This function gets called when timer with index "timerNumber" expires.
+* The function of this timer depends on the protocol programmer
+*/
 void timer_callback(int timerNumber) {
 
 }
